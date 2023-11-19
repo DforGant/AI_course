f3c78b8320fcc788c2c92294aa0533211cc42be9
DforGant <150796899+DforGant@users.noreply.github.com>
2023-11-19 16:55:52 +0800

    Update homework-1.py

--- homework-1.py
+++ homework-1.py
@@ -1,1 +1,12 @@
+num = input('Number: ')
+# 1 рубль, 2-4 рубля, 5-20 рублей
 
+for i in num:
+	list.append = i
+
+#last_Symbol = list[-1:0:0]
+#if last_Symbol 
+
+#print(num, 'рублей')
+
+print(list)
